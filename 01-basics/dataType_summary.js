d81d97e5c45e2c81e61b75a93bd76b4703313bcd
Aditya.A <132101791+Aditya07A@users.noreply.github.com>
2023-08-29 07:11:41 +0000

    upto stack& heap

--- 01-basics/dataType_summary.js
+++ 01-basics/dataType_summary.js
@@ -18,5 +18,24 @@ console.log(id1 ==== id2);//will get false
 # check datatype :-  console.log(typeof (x))// will get number
 type of null = object 
 type of bigInt = undefined
+---------------------------------------------------------------------
+stack memory used in primitive 
+heap used in non-primitive
  
 */
+let name1 = "abcd";
+let name2 = name1;
+name2 = "efgh";
+// console.log(name1);
+// console.log(name2);
+// here we get copy of the name1 from the stack but the otiginal value is same not changed
+
+let user1={
+    email : "abcd@gmail.com",
+    upi :"use@ybl"
+}
+let user2 = user1;
+user2.email = "qwerty@gmail.com";
+console.log(user1.email);
+console.log(user2.email);
+// here we get the reference of the value & if value changed both the value get effected.
