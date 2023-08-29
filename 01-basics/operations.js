b3ae8d66bdbbbc4bf95297fad67343051cd62e23
Aditya.A <132101791+Aditya07A@users.noreply.github.com>
2023-08-29 06:03:22 +0000

    update till comparisons

---
+++ 01-basics/operations.js
@@ -0,0 +1,23 @@
+let value = 1;
+let negValue = -value;
+//console.log(negValue);
+
+let str1 = "hello";
+let str2 =  " adi";
+let str3 = str1 + str2;
+//console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+//console.log(true+); error
+
+let counter = 100;
+counter++;
+console.log(counter);
+++counter;
+console.log(counter);
