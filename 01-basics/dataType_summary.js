a8861b78534aef99faaad7e2dbafc6f5f44db77d
Aditya.A <132101791+Aditya07A@users.noreply.github.com>
2023-08-29 06:26:44 +0000

    datatype-summary

---
+++ 01-basics/dataType_summary.js
@@ -0,0 +1,22 @@
+/*
+# Javascript is dynamically typed, as varibale type are detremined & checked at runtime
+   let x = 5; // x is a number  
+   x = "Hello"; // x is now a string
+   x = [1, 2, 3]; // x is now an array
+   const temp = null{null means not 0 but completely empty}
+
+# datatypes :- based the storage 2 types primitive & non-primitive(refrence type)
+
+# Primitive :- 7types - string,numbers,boolean,null,undefined,symbol,bigInt
+
+# Non-primitive:- arrays,objects,functions
+
+const id1 = Symbol("123");
+const id2 = Symbol("123");
+console.log(id1 ==== id2);//will get false
+
+# check datatype :-  console.log(typeof (x))// will get number
+type of null = object 
+type of bigInt = undefined
+ 
+*/
