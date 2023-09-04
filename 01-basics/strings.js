96786ea0fe7f1649b1e0d09ded6cca70d297b8c9
Aditya.A <132101791+Aditya07A@users.noreply.github.com>
2023-09-04 10:56:12 +0000

    updated strings

--- 01-basics/strings.js
+++ 01-basics/strings.js
@@ -12,16 +12,29 @@ const gameName1 = new String("qwerty");
 // console.log(gameName.indexOf("w"));
 // console.log(gameName.charAt(3));
 const gameName = new String("qwerty-uiop");
-console.log(gameName.length);
+//console.log(gameName.length);
 const newString = gameName.substring(0,4);
-console.log(newString);
+//console.log(newString);
 //index number 0-3 will be output ,4 is not include,& if given -ve value it will take as 0.
 const newString2 = gameName.slice(0,5)
-console.log(newString2);
+//console.log(newString2);
 //here it will print qwert
 const newString3 = gameName.slice(-11,4)
-console.log(newString3);
+//console.log(newString3);
 //output= qwe & it take in reverse order from the length of it.
 const gameName2 = new String("hitesh-hc");
 const newString4 = gameName2.slice(-8,4)
-console.log(newString4);
+//console.log(newString4);
+
+const newString1 = "   Aditya   ";
+// console.log(newString1);
+// console.log(newString1.trim());
+// console.log(newString1.trimStart());
+// console.log(newString1.trimEnd());
+
+const url = "http://aditya.com/aditya%20adiraju";
+console.log(url.replace("%20","-"));
+console.log(url.includes('qwerty'));
+
+const gName = new String("qwerty-uiop");
+console.log(gName.split("-"));
